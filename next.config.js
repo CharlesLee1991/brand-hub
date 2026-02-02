38ed49fae9c76fb57c9b5d82710658c31a1fa349
CharlesLee1991 <lcseung@bizspring.co.kr>
2026-02-02 23:29:38 +0900

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['nntuztaehnywdbttrajy.supabase.co'],
+  },
+}
+
+module.exports = nextConfig
